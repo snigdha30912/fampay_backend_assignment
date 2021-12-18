6664b065b32d7c518d7d947afae0e2ac50b05ea1
sniggo jain <snigdha.jain30@gmail.com>
2021-12-18 21:52:15 +0530

    created models for the application

--- searchapi/models.py
+++ searchapi/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-from django.conf import settings
-from django.contrib import auth
 from django.utils.timezone import now
 from django.utils import timezone
-import json
-from django.db.models.signals import post_save
 
 
-# Create your models here.
+"""
+Created Video Model 
+Created fields like Video title, Thumbnail URL, Publishing Date and Description
 
+"""
 
 class Video(models.Model):
     video_title = models.CharField(default="No Title", max_length=264)
