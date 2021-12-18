b0663723c54e7dd47d6193961125c1b8b56e5cb0
sniggo jain <snigdha.jain30@gmail.com>
2021-12-18 22:34:11 +0530

    created urls for all the views

--- searchapi/urls.py
+++ searchapi/urls.py
@@ -6,6 +6,11 @@ from django.urls import path
 
 app_name = 'searchapi'
 
+"""
+Created urls for all the views.
+
+"""
+
 urlpatterns = [
     path('searchApi/', views.VideoAPIView.as_view()),
     path('order/', views.VideoOrderAPIView.as_view()),
