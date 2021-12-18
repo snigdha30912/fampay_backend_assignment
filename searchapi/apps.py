cf9c976cb8edfd0d63f7816005af23ac4ab7f32e
sniggo jain <snigdha.jain30@gmail.com>
2021-12-18 22:15:48 +0530

    code to start the scheduler at the start of the application

--- searchapi/apps.py
+++ searchapi/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class SearchapiConfig(AppConfig):
     name = 'searchapi'
 
+    # starting the scheduler at the starting of the application
     def ready(self):
-        print("Starting Scheduler ...")
         from .youtube_scheduler import youtube_updater
         youtube_updater.start()
